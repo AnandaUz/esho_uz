2b170f3f9030a8775311e83a5ecedb243a819406
angel <angel.yarkiy@gmail.com>
2026-03-31 10:09:50 +0500

    +

--- client/src/pages/admin/admin.ts
+++ client/src/pages/admin/admin.ts
@@ -1,42 +1,98 @@
 import type { Page } from '../../types';
 import "./admin.scss";
 
-function updateStatus() {
-    const isDebug = localStorage.getItem('no_analytics') === 'true';
-    const statusElement = document.getElementById('status');
-    if (statusElement) {
-        statusElement.innerText = isDebug ? 'ЗАБЛОКИРОВАНО' : 'РАБОТАЕТ';
-        statusElement.style.color = isDebug ? 'red' : 'green';
+interface Btn {
+  name: string;
+  status: string;
+  title: string;
+  storageName: string;
+  isOff: boolean;
+  btnElement: HTMLButtonElement | null;
+}
+const btns: Btn[] = [
+  { name: 'btn_GTM', 
+    status: 'GTM', 
+    title: 'GoogleTagManager', 
+    storageName:'off_GTM',
+    isOff:false,
+    btnElement: null
+  },
+  { name: 'btn_Pixel', 
+    status: 'Pixel', 
+    title: 'Pixel', 
+    storageName:'off_Pixel',
+    isOff:false,
+    btnElement: null 
+  },
+  { name: 'btn_MyStat', 
+    status: 'MyStat', 
+    title: 'Моя статистика', 
+    storageName:'off_MyStat',
+    isOff:false,
+    btnElement: null
+  }
+]
+
+
+function getStorageStatus() {
+  btns.forEach(btn => {
+    const status = localStorage.getItem(btn.storageName);
+    if (status) {
+      btn.isOff = status === 'true';
+      if (btn.isOff) {
+        btn.btnElement?.classList.add('off');
+      } else {
+        btn.btnElement?.classList.remove('off');
+      }
     }
+  })
+  return localStorage.getItem('no_analytics');
 }
 
-function enableDebug() {
-    localStorage.setItem('no_analytics', 'true');
-    updateStatus();
 
-}
-
-function disableDebug() {
-    localStorage.removeItem('no_analytics');
-    updateStatus();
-}
 export const adminPage: Page = () => {
   return {
     html: `
     <div class="admin">
       <h1>Управление статистикой</h1>
-<p class="status">Текущий статус: <strong id="status">проверка...</strong></p>
 
-      <button class="btn_big">Включить режим разработчика (Блокировать GTM/Pixel)</button>
-      <button class="btn_big">Выключить режим разработчика (Разрешить отправку)</button>      
+<button class="btn_big btn_off_all">Выключить всё</button>
+<div class="btns">
+  
+</div>
+           
     </div>
     `,
     init() {
-      const btn1 = document.querySelector('button:nth-of-type(1)');
-      const btn2 = document.querySelector('button:nth-of-type(2)');
-      if (btn1) btn1.addEventListener('click', enableDebug);
-      if (btn2) btn2.addEventListener('click', disableDebug);
-      updateStatus();
+      document.querySelector('.btn_off_all')?.addEventListener('click', () => {
+        btns.forEach(btn => {
+          btn.isOff = true;
+          localStorage.setItem(btn.storageName, btn.isOff.toString());
+        })
+        getStorageStatus();
+      })
+
+      const btnsContainer = document.querySelector('.btns');
+
+      btns.forEach(btn => {
+        const btnElement = document.createElement('button');
+        btnElement.classList.add('btn_big');
+        btnElement.classList.add(btn.name);
+        btnElement.innerHTML = btn.title + '<span class="isOff">Выключено</span><span class="isOn">Включено</span>';
+        btn.btnElement = btnElement;
+
+       
+        btnElement.addEventListener('click', () => {
+          btn.isOff = !btn.isOff;
+          localStorage.setItem(btn.storageName, btn.isOff.toString());
+          getStorageStatus();
+        });
+        
+        if (btnsContainer) {
+          btnsContainer.appendChild(btnElement);
+        }
+      })   
+      getStorageStatus();
       
     },
     title: 'Админ'
