4fe75398050ad9dfe99fb15061edb44a0205c787
angel <angel.yarkiy@gmail.com>
2026-04-02 12:10:32 +0500

    +

--- client/src/pages/meet/meet.ts
+++ client/src/pages/meet/meet.ts
@@ -3,7 +3,7 @@ import "./meet.scss";
 import html from "./meet.html?raw";
 import '@components/top/c-top';
 import '@components/c-form-meet/c-form-meet';
-import '@components/c-anh-toBot';
+import '@/components/c-anh-to_bot';
 
 export const meetPage: Page = () => {
   return {
