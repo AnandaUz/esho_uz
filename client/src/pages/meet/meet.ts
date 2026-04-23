7c2929981cfae2d0940c752f52d1989fb4fa2b3b
angel <angel.yarkiy@gmail.com>
2026-04-23 17:57:07 +0500

    +

--- client/src/pages/meet/meet.ts
+++ client/src/pages/meet/meet.ts
@@ -1,17 +1,17 @@
-import type { Page } from '../../types';
+import type { Page } from "../../types";
 import "./meet.scss";
 import html from "./meet.html?raw";
-import '@components/top/c-top';
-import '@components/c-form-meet/c-form-meet';
-import '@/components/c-anh-to_bot';
+import "@components/top/c-top";
+import "@components/c-form-meet/c-form-meet";
+import "@/components/c-anh-to_bot";
 
 export const meetPage: Page = () => {
   return {
-    html: html,
-    init() {
-      
-      
-    },
-    title: 'Встреча'
+    html: html.replaceAll(
+      "{{VITE_TGBOT_MEET_URL}}",
+      import.meta.env.VITE_TGBOT_MEET_URL,
+    ),
+    init() {},
+    title: "Встреча",
   };
-}
+};
